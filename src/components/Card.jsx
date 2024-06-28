a2b740e845840b774cb07b99972db9f2eccbd633
PalmerCurrie <palmercurrie@gmail.com>
2024-06-28 14:57:59 -0700

    Working on Flip Animation For Card Shuffling

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,25 +1,49 @@
 /* eslint-disable react/prop-types */
+import { useEffect, useState } from 'react';
 import '../styles/Card.css'; // Import the CSS file
 
-export default function Card({pokemon, onClick}) {
+export default function Card({pokemon, onClick, isShuffling}) {
+    const [isFlipped, setIsFlipped] = useState(false);
+
+    useEffect(() => {
+        if (isShuffling) {
+            setIsFlipped(true);
+            setTimeout(() => {
+                setIsFlipped(false);
+            }, 500); 
+        }
+    }, [isShuffling]);
 
     const handleClick = () => {
-        onClick(); 
-      };
+        if (!isShuffling) {
+            onClick(); 
+        }
+    };
 
     return (
-        <div className="pokemon-card" id={pokemon.name} onClick={handleClick}>
+        <div className={`pokemon-card ${isFlipped ? 'flipped' : ''}`} id={pokemon.name} onClick={handleClick}>
+            <div className='pokemon-card-inner'>
+                <div className='pokemon-card-front'>
+                    <img
+                    src={pokemon.image}
+                    alt={pokemon.name}
+                    className="pokemon-image"
+                    draggable="false"
+                    />
 
-            <img
-              src={pokemon.image}
-              alt={pokemon.name}
-              className="pokemon-image"
-              draggable="false"
-            />
-
-            <p className='pokemon-name'>
-                {pokemon.name}
-            </p>
+                    <p className='pokemon-name'>
+                        {pokemon.name}
+                    </p>
+                </div>
+            </div>
+            <div className='pokemon-card-back'>                
+                <img
+                    src="/pokemon-card-back.png"
+                    alt="Pokemon Card Back"
+                    className="pokemon-image"
+                    draggable="false"
+                />
+            </div>
 
         </div>
 
